8999422cc145a6c61c8ce32cd10349a74a7aebdc
Taras Bulavin <tbul130599@gmail.com>
2023-01-19 18:51:19 +0200

    commit

--- HW/JSHW/1hw.js
+++ HW/JSHW/1hw.js
@@ -1,6 +1,15 @@
-const arr = [];
+{const arr = [];
 let numbers = 100;
 while(arr.length < numbers) {arr.push(Math.round(Math.random()*100))};
 function newArray( [ ] ){
- let arr2 = arr.filter((el)=> el>50).sort((a , b)=> a - b)};
- console.log(newArray(arr))
+ let arr2 = arr.filter((el)=> el>50).sort((a , b)=> a - b)
+ return arr2 }}
+
+ {const arr = [];
+   let numbers = 100;
+   let abc = "abc";
+   while(arr.length < numbers) {arr.push(abc)};
+   function NewObj(arr){
+   arr.sort((a,b)=> a - b);
+   let newObj = ({...arr});
+   return newObj};}
